c8738b9e82f4a56c5b86a1e2641592f4ab2a28fa
Stepan Kulagin <ed2k@bk.ru>
2022-09-26 18:22:10 +0300

    4.1.0. Оператор if с блоком else [#227534 #2129]

--- src/test/java/ru/job4j/MainTest.java
+++ src/test/java/ru/job4j/MainTest.java
@@ -6,7 +6,10 @@ import static org.assertj.core.api.Assertions.*;
 public class MainTest {
     @Test
     public void whenActionThenResult() {
-        int excepted = 1;
+
+
+
+       int excepted = 1;
         int result = 1;
         assertThat(result).isEqualTo(excepted);
     }
